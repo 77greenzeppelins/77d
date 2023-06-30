a3052ce250bdc2900db3a9784ebdb0eb6d3364d3
77greenzeppelins <75420866+77greenzeppelins@users.noreply.github.com>
2023-06-30 06:03:04 +0200

    pageCV_header-as-h3_1.0

--- components/pagesComponents/pageCV/pageContent/infoSection/header/SectionHeader.tsx
+++ components/pagesComponents/pageCV/pageContent/infoSection/header/SectionHeader.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SectionHeader = ({ text }: { text: string }) => {
   return (
-    <div className="w-full relative h-[28px]">
-      <p className="p-regular uppercase text-corpo font-serif">{text}</p>
+    <h3 className="w-full relative h-[28px]">
+      <p className="font-serif uppercase p-regular text-corpo">{text}</p>
       <span className="absolute inset-x-0 bottom-0  border-b border-corpo w-full h-[1px]" />
-    </div>
+    </h3>
   );
 };
 
